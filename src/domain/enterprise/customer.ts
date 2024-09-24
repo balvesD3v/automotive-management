1f39538c46b47dafe8d7597bf835ea24eaa56cfb
BalvesD3v <lpbalves7filho@gmail.com>
2024-09-24 17:55:14 -0300

    feat(entity customer): create entity customer

--- src/domain/enterprise/customer.ts
+++ src/domain/enterprise/customer.ts
@@ -1,4 +1,33 @@
-export enum Role {
-  PATIENT = 'patient',
-  DOCTOR = 'doctor',
+import { Entity } from 'src/core/entities/entity'
+import { UniqueEntityId } from 'src/core/entities/unique-entity-id'
+
+interface CustomerProps {
+  firstName: string
+  lastName: string
+  email: string
+  password: string
+}
+
+export class Customer extends Entity<CustomerProps> {
+  get firstName() {
+    return this.props.firstName
+  }
+
+  get lastName() {
+    return this.props.lastName
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  get password() {
+    return this.props.password
+  }
+
+  static create(props: CustomerProps, id?: UniqueEntityId) {
+    const customer = new Customer(props, id)
+
+    return customer
+  }
 }
